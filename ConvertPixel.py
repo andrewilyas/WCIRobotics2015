7751be4eef6323e3c1435054c1d8d5e29a5a8ea9
zarinaaa <zarinamamoukhova@gmail.com>
2015-03-13 16:07:12 -0700

    Convert Pixel

--- ConvertPixel.py
+++ ConvertPixel.py
@@ -5,16 +5,23 @@ y0 = 1
 f = 1
 h = 1 # height of the camera from the ground in cm
 Hpx = 1 # height of the sceen in pixels
-x = 1 #x is location of pixel on the screen
-def convert(h,Hpx, x, y, y0, f):
+Wpx = 1 #height of the screen in pixels
+yScreenDistance = 1 #is location of pixel on the screen
+xScreenDistance = 1
+def convert(h,Hpx, yScreenDistance, y, y0, f):
     R = math.sqrt( (h*h)+(y*y) )
     alpha = math.atan(h/y)
     
     a = ( ( h*y )-( y0*h ) ) / ( ( y0*y ) + h*h ) #half the height of the screen in cm
     Cpx = (2*a)/Hpx
-    A = math.cos(Hpx/2 -x)
+    A = math.cos(Hpx/2 - yScreenDistance)
     v = h / ((f*math.sin(alpha) + Cpx*(A)))
-    ym =  v*( f*math.cos(alpha)-( math.sin(alpha) * ((Hpx/2) -x) * Cpx ) )         
-    return ym
-ym = convert(h,Hpx,x,y,y0,f)
+    ym =  v*( f*math.cos(alpha)-( math.sin(alpha) * ((Hpx/2) - yScreenDistance) * Cpx ) )         
+    d = (Wpx/2 - xScreenDistance)
+    ymPrime = math.sqrt(h*h + ym*ym);
+    xm = d*ymPrime / f
+    return xm,ym
+xm,ym = convert(h,Hpx,yScreenDistance,y,y0,f)
 print ym
+print xm
+
