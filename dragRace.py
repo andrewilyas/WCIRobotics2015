77ce7d5544adedee347765b1fb154cfd2e24540d
rakkg <rakkgschool@gmail.com>
2015-03-13 15:24:20 -0400

    Added Line Detect to Main Codebase

--- dragRace.py
+++ dragRace.py
@@ -21,3 +21,50 @@ def polar2cart(rho, theta):     #converts polar to cart
     y = rho*np.sin(theta)
     return x,y
 
+def detectLines(src, threshold=5, cannyThreshold1=100, cannyThreshold2=100, houghLinesThreshold=200, startingSensitivity=100):
+
+    #Some other variables...
+    linesFound = 0
+    thresholdFound = False
+    newCannyThreshold1 = cannyThreshold1
+    newCannyThreshold2 = cannyThreshold2
+    sensitivity = startingSensitivity
+
+    #================[Actual Code]================#
+
+    while thresholdFound != True:
+        edges = cv2.Canny(cv2.cvtColor(src, CV_BGR2GRAY), threshold1=newCannyThreshold1, threshold2=newCannyThreshold2,apertureSize = 3)
+        lines = cv2.HoughLines(edges,1,CV_PI/180,houghLinesThreshold)
+
+        errorFound = False
+        try:
+            #Count the lines
+            for rho,theta in lines[0]:
+                linesFound = linesFound + 1
+
+        except (TypeError):
+            #No lines found so more sensitivity please...
+            errorFound = True
+            if(sensitivity <= 10):
+                print "Sensitivity too low to adjust further, aborting..."
+                errorFound = True
+                thresholdFound = True
+            else:
+                #Increase sensitivity
+                print "Sensitivity too low! Increasing sensitivity by 10 (new sensitivity is: " + str(sensitivity) + ")"
+                sensitivity = sensitivity - 10 #The "-" here is not a mistake, the sensitivity is backwards...
+                newCannyThreshold1 = cannyThreshold1
+                newCannyThreshold2 = cannyThreshold2
+
+        if linesFound <= threshold and errorFound == False:
+            thresholdFound = True
+            print "Jackpot buddy, we've got the right amount of lines: " + str(linesFound)
+
+            #WE ARE DONE :)
+            return lines[0]
+        else:
+            if errorFound == False:
+                print "Too many lines (" + str(linesFound) + ") continuing search!"
+                newCannyThreshold1 = newCannyThreshold1 + sensitivity
+                newCannyThreshold2 = newCannyThreshold2 + sensitivity
+                linesFound = 0
