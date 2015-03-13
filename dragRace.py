035f6797bc0e0125a41b79e04b5ef3dd5462654c
rakkg <rakkgschool@gmail.com>
2015-03-13 15:22:24 -0400

    Imshow fixes

--- dragRace.py
+++ dragRace.py
@@ -6,17 +6,17 @@ c = cv2.VideoCapture(0) #Initialize the video capture instance, 0 refers to the 
 while True:
     _,f = c.read() #Getting the image from the camera
     f = cv2.cvtColor(f, CV_BGR2GRAY)
-    cv2.imshow("Webcam feed", f) #Show the image with Title "Webcam Feed"
     arrCoords = lineDetect(f)
     for line in arrCoords:
-        x, y = polar2cart(line[0], line[1])
+        x, y = polar2cart(line[0], line[1]) #Calls the polar2cart function        
         x1, x2, y1, y2 = x[0], x[1], y[0], y[1]
         cv2.line(f, (x1, y1), (x2, y2), (0,0,0))
+    cv2.imshow("Webcam feed", f) #Show the image with Title "Webcam Feed"
     if cv2.waitKey(5) == 27: #Wait for 5ms for a Ctrl+W, break if found
         break
 cv2.destroyAllWindows()
 
-def polar2cart(rho, theta):
+def polar2cart(rho, theta):     #converts polar to cart
     x = rho*np.cos(theta)
     y = rho*np.sin(theta)
     return x,y
