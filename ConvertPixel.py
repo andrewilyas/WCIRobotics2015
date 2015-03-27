7eee334aea0562165cf4171d9184e6a304b59d7d
Andrew Ilyas <andyilyas@gmail.com>
2015-03-26 20:41:43 -0400

    Slope

--- ConvertPixel.py
+++ ConvertPixel.py
@@ -2,7 +2,7 @@ import math
 #given
 y = 1
 y0 = 1
-f = 1
+f = 2.9
 h = 1 # height of the camera from the ground in cm
 Hpx = 1 # height of the sceen in pixels
 Wpx = 1 #height of the screen in pixels
@@ -11,12 +11,12 @@ xScreenDistance = 1
 def convert(h,Hpx, yScreenDistance, y, y0, f):
     R = math.sqrt( (h*h)+(y*y) )
     alpha = math.atan(h/y)
-    
+
     a = ( ( h*y )-( y0*h ) ) / ( ( y0*y ) + h*h ) #half the height of the screen in cm
     Cpx = (2*a)/Hpx
     A = math.cos(Hpx/2 - yScreenDistance)
     v = h / ((f*math.sin(alpha) + Cpx*(A)))
-    ym =  v*( f*math.cos(alpha)-( math.sin(alpha) * ((Hpx/2) - yScreenDistance) * Cpx ) )         
+    ym =  v*( f*math.cos(alpha)-( math.sin(alpha) * ((Hpx/2) - yScreenDistance) * Cpx ) )
     d = (Wpx/2 - xScreenDistance)
     ymPrime = math.sqrt(h*h + ym*ym);
     xm = d*ymPrime / f
--- dragRace.py
+++ dragRace.py
@@ -10,7 +10,7 @@ def polar2cart(rho, theta):     #converts polar to cart
     y2 = rho*np.sin(theta) - 1000*np.cos(theta)
     return [x, x2], [y, y2]
 
-def detectLines(src, threshold=5, cannyThreshold1=100, cannyThreshold2=100, houghLinesThreshold=200, startingSensitivity=100):
+def detectLines(src, threshold=5, cannyThreshold1=100, cannyThreshold2=100, houghLinesThreshold=100, startingSensitivity=100):
     #Some other variables...
     linesFound = 0
     thresholdFound = False
@@ -22,53 +22,57 @@ def detectLines(src, threshold=5, cannyThreshold1=100, cannyThreshold2=100, houg
 
     while thresholdFound != True:
         edges = cv2.Canny(cv2.cvtColor(src, CV_BGR2GRAY), threshold1=newCannyThreshold1, threshold2=newCannyThreshold2,apertureSize = 3)
-        lines = cv2.HoughLines(edges,1,CV_PI/180,houghLinesThreshold)
-
+        lines = cv2.HoughLinesP(edges,1,CV_PI/180,houghLinesThreshold, minLineLength=50)
         errorFound = False
         try:
             #Count the lines
-            for rho,theta in lines[0]:
+            for x1,y1,x2,y2 in lines[0]:
                 linesFound = linesFound + 1
 
         except (TypeError):
             #No lines found so more sensitivity please...
             errorFound = True
             if(sensitivity <= 10):
-                print "Sensitivity too low to adjust further, aborting..."
+                #print "Sensitivity too low to adjust further, aborting..."
                 errorFound = True
                 thresholdFound = True
             else:
                 #Increase sensitivity
-                print "Sensitivity too low! Increasing sensitivity by 10 (new sensitivity is: " + str(sensitivity) + ")"
+                #print "Sensitivity too low! Increasing sensitivity by 10 (new sensitivity is: " + str(sensitivity) + ")"
                 sensitivity = sensitivity - 10 #The "-" here is not a mistake, the sensitivity is backwards...
                 newCannyThreshold1 = cannyThreshold1
                 newCannyThreshold2 = cannyThreshold2
 
         if linesFound <= threshold and errorFound == False:
             thresholdFound = True
-            print "Jackpot buddy, we've got the right amount of lines: " + str(linesFound)
+            #print "Jackpot buddy, we've got the right amount of lines: " + str(linesFound)
 
             #WE ARE DONE :)
             return lines[0]
         else:
             if errorFound == False:
-                print "Too many lines (" + str(linesFound) + ") continuing search!"
+                #print "Too many lines (" + str(linesFound) + ") continuing search!"
                 newCannyThreshold1 = newCannyThreshold1 + sensitivity
                 newCannyThreshold2 = newCannyThreshold2 + sensitivity
                 linesFound = 0
+            else:
+                return [[0,0,0,0]]
 
 c = cv2.VideoCapture(0) #Initialize the video capture instance, 0 refers to the webcam number
 while True:
     _,f = c.read() #Getting the image from the camera
-    print f
+    #print f
     arrCoords = detectLines(np.asarray(f[:,:]))
     for line in arrCoords:
-        print line
-        x, y = polar2cart(line[0], line[1]) #Calls the polar2cart function        
-        x1, x2, y1, y2 = x[0], x[1], y[0], y[1]
-        cv2.line(f, (x1, y1), (x2, y2), (0,0,0))
+        #print line
+        x1, y1,x2, y2 = line
+        slope = 0
+        if abs(x2 - x1) > 0:
+            slope = float(y2 - y1)/float(x2 - x1) + 0.0
+            print slope
+        cv2.line(f, (x1, y1), (x2, y2), (0,0,0), thickness=3)
     f = cv2.cvtColor(f, CV_BGR2GRAY)
     cv2.imshow("Webcam feed", f) #Show the image with Title "Webcam Feed"
-    if cv2.waitKey(5) == 27: #Wait for 5ms for a Ctrl+W, break if found
+    if cv2.waitKey(5) == 27: #Wait for 5ms for an Esc, break if found
         break
 cv2.destroyAllWindows()
