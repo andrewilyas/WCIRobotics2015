6bcfa728a8894335ab897973b6df52a7fd059bf7
null-dev <contactnulldev@gmail.com>
2015-05-08 15:13:32 -0400

    Add adata

--- circuitrace.py
+++ circuitrace.py
@@ -9,4 +9,4 @@ def main():
         y1 = (lines[0][0][1]+lines[1][0][1])/2
         x2 = (lines[0][1][0]+lines[1][1][0])/2
         y2 = (lines[0][1][1]+lines[1][1][1])/2
-        slope = 
+        slope = (x1-x2)/(y2-y1)
