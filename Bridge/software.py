2ea61008562e131af5066fd1800f01445b6792bc
Sam Sun <samczsun@gmail.com>
2015-07-08 13:58:27 -0400

    New bridge with nanpy

--- Bridge/software.py
+++ Bridge/software.py
@@ -1,136 +1,72 @@
-import serial
-import time
-import traceback
-import struct
-from enum import Enum
+from nanpy import ArduinoApi, SerialManager, Servo
 
-class _command(Enum):
-    SOFTWARE_CONNECT = '\xff';
-    HARDWARE_ACKNOWLEDGE = '\xfe';
+class Pins():
+    DRIVE = 12
+    CAMERA = 11
+    TURN = 9
 
-    TOGGLE_LED = '\x03'
-    MOVE_CAMERA = '\x04'
-    TURN = '\x05'
-    DRIVE = '\x06'
-    STOP = '\x07'
-    READ_ULTRASONIC = '\x08'
+class Constants():
+    CAMERA_UP = 170
+    CAMERA_DOWN = 180
+    LEFT = 110
+    RIGHT = 60
 
-class Position(Enum):
+class Position():
     UP = 0
     DOWN = 1
 
-class Direction(Enum):
+class Direction():
     LEFT = 0
     RIGHT = 1
     FORWARD = 0
     BACKWARD = 1
 
 class Arduino:
-    def __init__(self, port):
-        print "Initializing Arduino on " + port
-        self.port = port
-        self.connect()
-
-    def connect(self, baud=28800):
-        if not hasattr(self, 'con'):
-            print "Connecting to Arduino with baud " + str(baud)
-            self.con = serial.Serial(self.port, baud)
-            self.handshake()
-            self.con.timeout = 0
-            print "Connect to Arduino"
-        else:
-            raise Exception("Already Connected")
-
-    def handshake(self):
-        self.con.timeout = 0.1;
-        self.con.write(_command.SOFTWARE_CONNECT.value)
-        handshake = self.con.read(size=1)
-        while len(handshake) != 1:
-            self.con.write(_command.SOFTWARE_CONNECT.value)
-            handshake = self.con.read(size=1)
-        if handshake != _command.HARDWARE_ACKNOWLEDGE.value:
-            raise Exception("Invalid response!")
-
-    def awaitAcknowledgement(self, command, callback):
-        while self.con.inWaiting() == 0:
-            #print "WAAAAAAAAAITING"
-            pass
-        response = self.con.read(size=self.con.inWaiting())
-        while ord(response[-1]) != 255:
-            response = response + self.con.read(size=self.con.inWaiting())
-        if response[0] == command:
-            argslen = i[1]
-            if argslen > 0:
-                args = []
-                for x in range(2, len(response) - 1):
-                    args.append(ord(response[x]))
-                self.invoke(callback, args)
-            return True
-        else:
-            return False
-
-    def sendCommand(self, command, data=[], callback=None):
-        self.con.write(command.value)
-        string = struct.pack('!B', len(data))
-        for i in tuple(data):
-            string += struct.pack('!B', i)
-        self.con.write(string)
-        if self.awaitAcknowledgement(command.value, callback):
-            return True
-        else:
-            raise Exception("Could not send command " + command)
-
-    def disconnect(self):
-        self.con.close()
-        print "Disconnected"
-
-    def toggleLed(self, pin, callback=None):
-        self.sendCommand(_command.TOGGLE_LED, data=[pin], callback=callback)
-
+    def __init__(self):
+        print "Initializing Arduino"
+        self._serialManager = SerialManager()
+        self._arduino = ArduinoApi(connection = self._serialManager)
+        self._driveServo = Servo(Pins.DRIVE, connection = self._serialManager)
+        self._cameraServo = Servo(Pins.CAMERA, connection = self._serialManager)
+        self._turnServo = Servo(Pins.TURN, connection = self.serialManager)
+        
     def moveCamera(self, position):
-        self.sendCommand(_command.MOVE_CAMERA, data=[position.value])
+        if position == Position.UP:
+            self._cameraServo.write(Constants.CAMERA_UP)
+        elif position == Position.DOWN:
+            self._cameraServo.write(Constants.CAMERA_DOWN)
+        elif:
+            raise Exception("Unknown camera position")
 
-    def turn(self, direction, degrees):
-        self.sendCommand(_command.TURN, data=[degrees.value])
+    def turn(self, direction):
+        if position == Direction.LEFT:
+            self._turnServo.write(Constants.LEFT)
+        elif position == Direction.RIGHT:
+            self._turnServo.write(Constants.RIGHT)
+        elif:
+            raise Exception("Unknown direction")
 
     def drive(self, direction, speed):
-        if direction == Direction.BACKWARD:
-            speed = -1 * speed
-        self.sendCommand(_command.DRIVE, data=[speed])
+        if direction == Direction.FORWARD:
+            speed = 90 + speed
+        elif direction == Direction.BACKWARD:
+            speed = 90 - speed
+        else:
+            raise Exception("Unknown direction")
+        self._driveServo.write(speed)
 
     def stop(self):
-        self.sendCommand(_command.STOP)
-
-    def readUltrasonic(self, callback):
-        self.sendCommand(_command.READ_ULTRASONIC, callback=callback)
-
-    def invoke(self, function, *args):
-        if function != None:
-            function(*args)
+        self._driveServo.write(0)
 
 def printresults(l):
     for i in l:
         print "Got: " + str(i)
 
 def main():
-    arduino = Arduino("/dev/tty.usbmodem1411")
+    arduino = Arduino()
     while True:
-        try:
-            x = raw_input("Command\n> ")
-            if x == "drive forward":
-                arduino.toggleLed(13)
-                #speed = int(input("Speed\n> "))
-                #arduino.drive(Direction.FORWARD, speed)
-            elif x == "drive backward":
-                speed = int(input("Speed\n> "))
-                arduino.drive(Direction.BACKWARD, speed)
-            elif x == "stop":
-                arduino.stop()
-            else:
-                break
-        except Exception, e:
-            print traceback.format_exc()
-            break
+        x = int(input("Pin\n> "))
+        arduino.toggleLed(x, callback=printresults)
     arduino.disconnect()
 
 if __name__ == "__main__":
