290394ba37b938ab01529e83b7cb3db5a24c9ad4
Andrew Ilyas <ailyas@mit.edu>
2015-07-12 19:48:39 -0400

    Minor fixes

--- drag_race.py
+++ drag_race.py
@@ -8,11 +8,10 @@ import cv2
 import time
 import atexit
 
-cap = None
+cap, cam = Camera.camera_capture_instance(capture_mode)
 
 def main_loop():
     capture_mode = str(sys.argv[1])
-    cap, cam = Camera.camera_capture_instance(capture_mode)
     iterator = Camera.get_frame_iterator(capture_mode, cap, cam)
     h = Heuristics()
     time.sleep(0.1)
