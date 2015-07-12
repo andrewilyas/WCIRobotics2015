3953c6e61efb69f04316e1ab8239986c5d427cd0
rakkg <itsrakkg@gmail.com>
2015-07-12 19:39:56 -0400

    Arduino defined functions

--- arduino.py
+++ arduino.py
@@ -8,16 +8,14 @@ drive = Servo(12, connection = serial)
 turn = Servo(9, connection = serial)
 camera = Servo(11, connection = serial)
 
-while True:
-	action, speed = raw_input("gib acton pl0x\n> ").split()
-	speed = int(speed)
-	if action == "drive":
-		drive.write(speed + 90)
-	elif action == "turn":
-		turn.write(speed)
-	elif action == "stop":
-		drive.write(90)
-	elif action == "camera":
-		camera.write(speed)
-	else:
-		break
+def drive (speed):
+	drive.write(speed + 90)
+
+def turn(speed):
+	turn.write(speed)
+
+def stop():
+	drive.write(90)
+
+def camera(speed):
+	camera.write(speed)
